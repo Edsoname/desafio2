d8f9ff576678889513601e32adbc36a7411c5e7c
codigo de automação teste 1 <edson451.ea@gmail.com>
2024-02-16 22:26:53 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import pyautogui
+
+pyautogui.rightClick(214,309, duration=1)
+pyautogui.click()
+pyautogui.moveTo(778,290,duration=2)
+pyautogui.dragTo(464,345,button='left',duration=1.5)
+pyautogui.rightClick(296,124, duration=1)
+pyautogui.click()
+pyautogui.rightClick(142,383, duration=1)
+pyautogui.click()
+pyautogui.rightClick(520,463, duration=1)
+pyautogui.click()
+
+
+
+
+
